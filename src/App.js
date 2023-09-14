3af2f68ffea43770d7dfa663c5cfbca946b8cd0a
Nhan Tran <rim25102004@gmail.com>
2023-09-14 19:10:03 +0300

    Third Nhan Tran's Portfolio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes';
-import { DefaultLayout } from './components/Layout';
-
+import { publicRoutes } from '@/routes';
+import Layout from './pages/Layout';
 
 function App() {
   return (
-   <Router>
-    <div className='App'>
-      <Routes>
-          {publicRoutes.map((route,index)=> {
-            const Layout = route.layout || DefaultLayout;
-            const Page = route.component;
-            return <Route 
-            key={index}
-            path={route.path} 
-            element={
-       
-              <Page/>
-           
-            }/>
-          })}
-      </Routes>
+    <Router>
+    <div className="App">
+        <Routes>
+            {publicRoutes.map((route, index) => {
+                const Page = route.component;
+                let Lay = Layout;
+
+                if (route.layout) {
+                    Lay = route.layout;
+                } else if (route.layout === null) {
+                    Lay = Fragment;
+                }
+
+                return (
+                  
+                    <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                          <Lay>
+                              <Page />  
+                          </Lay>
+                          
+                        }
+                    />
+                );
+            })}
+        </Routes>
     </div>
-   </Router>
+</Router>
   );
 }
 
